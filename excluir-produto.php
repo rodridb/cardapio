05bd2bbd2a5d37873388f27cd315941d1305a8fc
rodriMA17 <107268316+rodriMA17@users.noreply.github.com>
2023-10-04 11:30:02 -0300

    commit inicial do projeto

---
+++ excluir-produto.php
@@ -0,0 +1,9 @@
+<?php
+
+require './src/conexao-bd.php';
+require './src/Modelo/Produto.php';
+require './src/Repositorio/ProdutoRepositorio.php';
+
+$produtoRepositorio = new ProdutoRepositorio($pdo);
+$produtoRepositorio->deletar($_GET['id']);
+
---
+++ src/Repositorio/ProdutoRepositorio.php
@@ -0,0 +1,68 @@
+<?php
+
+class ProdutoRepositorio
+{
+    private PDO $pdo;
+
+    /**
+     * @param PDO $pdo
+     */
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    private function formarObjeto($dados)
+    {
+        return new Produto($dados['id'],
+            $dados['tipo'],
+            $dados['nome'],
+            $dados['descricao'],
+            $dados['imagem'],
+            $dados['preco']);
+    }
+
+    public function opcoesCafe(): array
+    {
+        $sql1 = "SELECT * FROM produtos WHERE tipo = 'Café' ORDER BY preco";
+        $statement = $this->pdo->query($sql1);
+        $produtosCafe = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        $dadosCafe = array_map(function ($cafe){
+            return $this->formarObjeto($cafe);
+        },$produtosCafe);
+
+        return $dadosCafe;
+    }
+    public function opcoesAlmoco(): array
+    {
+        $sql2 = "SELECT * FROM produtos WHERE tipo = 'Almoço' ORDER BY preco";
+        $statement = $this->pdo->query($sql2);
+        $produtosAlmoco = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        $dadosAlmoco = array_map(function ($almoco){
+            return $this->formarObjeto($almoco);
+        },$produtosAlmoco);
+
+        return  $dadosAlmoco;
+    }
+
+    public function buscarTodos(){
+        $sql = "SELECT * FROM produtos ORDER BY preco";
+        $statement = $this->pdo->query($sql);
+        $dados = $statement->fetchAll(PDO::FETCH_ASSOC);
+        
+        $todosOsDados= array_map(function ($produtos){
+            return $this->formarObjeto($produtos);
+        },$dados);
+        return $todosOsDados; 
+    }
+
+    public function deletar(int $id)
+    {
+        $sql = "DELETE FROM produtos WHERE id = ?";
+        $statement = $this->pdo->prepare($sql);
+        $statement->bindValue(1, $id);
+        $statement->execute();
+    }
+}
